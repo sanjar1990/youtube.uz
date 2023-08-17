b23631213d1bb64c5b39988ef87a405910f75013
sanjar1990 <sanjar902105@gmail.com>
2023-08-17 19:41:01 +0500

    flyaway config and category insert

---
+++ src/main/resources/db/migration/V1.1__insert_category.sql
@@ -0,0 +1,14 @@
+insert into category(name,visible,created_date)
+values ('films',true,now());
+insert into category(name,visible,created_date)
+values ('Cartoon',true,now());
+insert into category(name,visible,created_date)
+values ('Clips',true,now());
+insert into category(name,visible,created_date)
+values ('Java',true,now());
+insert into category(name,visible,created_date)
+values ('Education',true,now());
+insert into category(name,visible,created_date)
+values ('FOOD',true,now());
+insert into category(name,visible,created_date)
+values ('High Tech',true,now());
